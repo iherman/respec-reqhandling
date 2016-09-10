bb5877e114374ccddfa89fce2b1649c7c1d21973
Ivan Herman <ivan@ivan-herman.net>
2016-09-10 17:32:33 +0200

    First version

---
+++ examples/reqInfo.js
@@ -0,0 +1,20 @@
+var reqInfo = [
+	{
+		"number": 1,
+		"content": "This is my first requirement",
+		"title": "Req.&nbsp;1",
+		"id": "first"
+	},
+	{
+		"number": 2,
+		"content": "This is my second requirement",
+		"title": "Req.&nbsp;2",
+		"id": "second"
+	},
+	{
+		"number": 3,
+		"content": "This is my third requirement",
+		"title": "Req.&nbsp;3",
+		"id": "third"
+	}
+]
